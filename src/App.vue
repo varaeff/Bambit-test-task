d429d91569b135e48b8ccb793da9b8cc3b956106
varaeff <varaev.sergey@gmail.com>
2025-03-17 15:50:08 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div class="text-center text-2xl font-bold text-blue-500">Tailwind test</div>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
