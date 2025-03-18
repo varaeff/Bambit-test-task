358f6a35199b99668a6d3d41f6501e6a9a1970a5
varaeff <varaev.sergey@gmail.com>
2025-03-18 22:19:15 +0400

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Bambit-test-task/',
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
